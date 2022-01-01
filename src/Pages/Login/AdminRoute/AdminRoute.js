6f7304928c103d8714ce8c082e58ba5398e8b8cf
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-01-02 00:06:45 +0600

    admin

--- src/Pages/Login/AdminRoute/AdminRoute.js
+++ src/Pages/Login/AdminRoute/AdminRoute.js
@@ -8,7 +8,7 @@ const AdminRoute = ({ children, ...rest }) => {
     const [isAdmin, setIsAdmin] = useState(true);
 
     useEffect(() => {
-        fetch(`https://fast-earth-44959.herokuapp.com/checkAdmin/${user?.email}`)
+        fetch(`http://localhost:3000/checkAdmin/${user?.email}`)
             .then((res) => res.json())
             .then((data) => {
                 if (data[0]?.role === "admin") {
