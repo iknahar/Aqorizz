387425e6180346b6195bd98e04d69817e2e560e9
Rohul Amin <rohulamin83716@gmail.com>
2022-01-01 23:43:14 +0600

    navar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from './Pages/Login/Login/Login';
-import Register from './Pages/Login/Register/Register';
-import AuthProvider from './contexts/AuthProvider/AuthProvider';
-
-
-
-
-
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import AuthProvider from "./contexts/AuthProvider/AuthProvider";
+import Header from "./compoent/Shared/Header";
 
 function App() {
- 
   return (
     <div className="App">
       <AuthProvider>
         <Router>
-       
-        <Switch>
-        
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-       
-        </Switch>
-      </Router>
+          <Header></Header>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
       </AuthProvider>
     </div>
   );
